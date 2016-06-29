734ad7b6b8e5268c5d4762bf10804cca35b0c1f1
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2016-06-29 15:58:22 -0500

    voting app FINISHED

--- src/voting/increment-vote.js
+++ src/voting/increment-vote.js
@@ -1,16 +1,16 @@
-export default function addOption(existingPoll, optionIndex) {
-  const collector = [];
-  let newObject={};
+export default function incrementVote(existingPoll, optionIndex) {
+  let collector = [];
+  let newObject= {};
 
   for (let i = 0; i < existingPoll.length; i++) {
-    collector.push(existingPoll[i]);
+    // newObject.name = `${existingPoll[i].name}`;
+    // newObject.votes = existingPoll[i].votes;
+
+    newObject = { name: `${existingPoll[i].name}`, votes: existingPoll[i].votes };
+    collector.push(newObject);
+
+    }
+
+    collector[optionIndex].votes++;
+    return collector;
   }
-
-newObject=collector.push(existingPoll[optionIndex].votes);
-
- collector.push(newObject);
- return newObject;
-
-
-
-}
